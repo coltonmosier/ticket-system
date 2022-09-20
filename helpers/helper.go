792d0bd3a480f9c5c5d6f5b7c782897ab07f257e
Colton Mosier <coltonmosier44@gmail.com>
2022-09-19 22:38:40 -0500

    create priority queue

--- helpers/helper.go
+++ helpers/helper.go
@@ -1,8 +1,13 @@
 package helpers
 
+// returns true/false if ticket exists
 func TicketExists() bool {
 
-	// returns true/false if ticket exists
-
 	return false
+}
+
+// function to test errors and return a string version of error
+func Errors(err error) string {
+
+	return ""
 }
---
+++ pkg/priorityq.go
@@ -0,0 +1,41 @@
+package pkg
+
+// file for priority queue functions
+import (
+	"container/heap"
+	"github.com/coltonmosier/models"
+)
+
+type PriorityQueue []*Ticket
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the lowest based on expiration number as the priority
+	// The lower the expiry, the higher the priority
+	return pq[i].Expiry < pq[j].Expiry
+}
+
+// We just implement the pre-defined function in interface of heap.
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.Index = -1
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Ticket)
+	item.Index = n
+	*pq = append(*pq, item)
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].Index = i
+	pq[j].Index = j
+}
