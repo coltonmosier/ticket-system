2d78be9fbdb03d97d2ee344b3156b1f5386db12b
Colton Mosier <coltonmosier44@gmail.com>
2022-09-22 08:32:15 -0500

    complete refactor to use cobra

---
+++ cmd/root.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "ticket-service",
+	Short: "Check and Create Tickets",
+	Long: `ticket-service is used to submit IT Tickets with different options
+    based on the catagory. Once submitted, the IT team will be able to read over
+    the tickets to best fix any issues that have arrived.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ticket-service.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2022 Colton Mosier<coltonmosier44@gmail.com>
+*/
+package main
+
+import "github.com/coltonmosier/ticket-service/cmd"
+
+func main() {
+	cmd.Execute()
+}
