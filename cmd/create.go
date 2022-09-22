9d619c0f720efc7b8ff1a1943cafa2d70450772a
Colton Mosier <coltonmosier44@gmail.com>
2022-09-22 13:17:55 -0500

    finish create function

--- cmd/create.go
+++ cmd/create.go
@@ -4,13 +4,28 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"os"
+	"strconv"
+	"time"
 
-	"github.com/coltonmosier/ticket-service/ticket"
 	"github.com/spf13/cobra"
 )
 
-var newTicket ticket.Ticket
+type ticket struct {
+	ID           int
+	User         string
+	Category     string
+	Description  string
+	UpdateReason string
+	Completed    bool
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}
+
+type tickets []ticket
 
 // createCmd represents the create command
 var createCmd = &cobra.Command{
@@ -22,6 +37,9 @@ var createCmd = &cobra.Command{
     User: <username>`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("create called")
+		newTicket := ticket{}
+		createTicket(&newTicket)
+		writeTicket(newTicket)
 	},
 }
 
@@ -29,6 +47,42 @@ func init() {
 	rootCmd.AddCommand(createCmd)
 }
 
-func (t *ticket.Ticket) createTicket() {
+func createTicket(t *ticket) {
+	var user string
+	var cat string
+	var id int
+	scan := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter ID: ")
+	fmt.Scan(&id)
+	fmt.Print("Enter username: ")
+	fmt.Scanln(&user)
+	fmt.Print("Enter category [password_help, network_issue, other]: ")
+	fmt.Scanln(&cat)
+	fmt.Print("Enter description: ")
+	desc, _ := scan.ReadString('\n')
+
+	newTicket := ticket{
+		ID:          id,
+		User:        user,
+		Category:    cat,
+		Description: desc,
+		Completed:   false,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Time{},
+	}
+
+	*t = newTicket
+
+}
+
+func writeTicket(t ticket) {
+	id := t.ID
+	data, err := json.Marshal(t)
+	if err != nil {
+		fmt.Println("error marshalling data")
+	}
+	file := "data/ticket" + strconv.Itoa(id) + ".json"
+
+	err = os.WriteFile(file, data, 0644)
 
 }
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ Copyright © 2022 Colton Mosier<coltonmosier44@gmail.com>
 */
 package main
 
-import "github.com/coltonmosier/ticket-service/cmd"
+import (
+	"github.com/coltonmosier/ticket-service/cmd"
+)
 
 func main() {
 	cmd.Execute()
